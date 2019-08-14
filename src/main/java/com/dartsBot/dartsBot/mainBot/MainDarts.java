6ff03d9ca17e78ca6535b2396b795b04f702b50f
d.voznyuk <d.voznyuk92@mail.ru>
2019-08-14 13:11:14 +0400

    refactoring

--- src/main/java/com/dartsBot/dartsBot/mainBot/MainDarts.java
+++ src/main/java/com/dartsBot/dartsBot/mainBot/MainDarts.java
@@ -24,6 +24,18 @@ public class MainDarts extends TelegramLongPollingBot {
     @Autowired
     private MatchRepository matchRepo;
 
+//    private static DefaultBotOptions setOptions() {
+//        DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
+//        botOptions.setProxyHost("45.13.30.144");
+//        botOptions.setProxyPort(60079);
+//        botOptions.setProxyType(DefaultBotOptions.ProxyType.SOCKS5);
+//        return botOptions;
+//    }
+//
+//    public MainDarts() {
+//        super(setOptions());
+//    }
+
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage()) {
@@ -42,15 +54,3 @@ public class MainDarts extends TelegramLongPollingBot {
 }
 
 
-//--------------for using in Russia--------------------------------------------
-//    private static DefaultBotOptions setOptions(){
-//        DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
-//        botOptions.setProxyHost("45.13.30.144");
-//        botOptions.setProxyPort(60079);
-//        botOptions.setProxyType(DefaultBotOptions.ProxyType.SOCKS5);
-//        return botOptions;
-//    }
-//
-//    public MainDarts(){
-//        super(setOptions());
-//    }
