e2bc3a8306c50cc9a20681eb9e0666fcd3c3d816
d.voznyuk <d.voznyuk92@mail.ru>
2019-08-19 16:59:38 +0400

    fix bags

--- src/main/java/com/dartsBot/dartsBot/exceptions/DartsErrors.java
+++ src/main/java/com/dartsBot/dartsBot/exceptions/DartsErrors.java
@@ -7,6 +7,7 @@ public class DartsErrors {
     public final static String WRONG_PLAYERS_COUNT = "Гораздо интересней играть в дартс вдвоем! Введите два имя через пробел";
     public final static String WRONG_COMMAND = "Комманда не распознана!";
     public final static String GAME_NOT_CREATE = "Вам неоткуда выходить. Вы не создавали игру";
-    public final static String WRONG_NAME_SYMBOLS = "Наши имена пишутся без спецсимволов или цифр)";
+    public final static String WRONG_NAME_SYMBOLS = "Наши имена пишутся без спецсимволов или цифр." +
+            " Если вы пытались ввести счет то пишите букву и 3 цифры !через пробел!)";
     public final static String SAME_PREF = "Непонимаю кому записать очки. У них имена одинаково начинаются";
 }
